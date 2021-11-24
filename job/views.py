3722ae6713ede842cc488de2fd5819d805e07e67
KhaledAnaqwa <khaled.anaqwa@gmail.com>
2021-11-24 22:04:56 +0200

    render simple data from using veiws & models

--- job/views.py
+++ job/views.py
@@ -7,8 +7,13 @@ from .models import Job,Category
 # Create your views here.
 
 def ListJobs(request):
-    return HttpResponse("ListJobs")
+    list = Job.objects.all()
+    # return HttpResponse(list)
+    context={'jobs':list}
+    return render(request,"job/JobList.html",context=context)
 
 def JobDetails(request,id):
-    return HttpResponse("JobDetails"+str(id))
+    job = Job.objects.get(id__exact=id)
+    context={'job':job}
+    return render(request,"job/JobDetails.html",context=context)
  
