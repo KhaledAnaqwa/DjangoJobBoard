bacc053d66401fe070a5ff050355908b7559fbfa
KhaledAnaqwa <khaled.anaqwa@gmail.com>
2021-11-25 14:46:10 +0200

    fill pages data + add pagination

--- job/urls.py
+++ job/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path,include
 from . import views
 
+app_name="job"
 urlpatterns = [
     path('',views.ListJobs),
-    path('<int:id>',views.JobDetails)
+    path('<int:id>',views.JobDetails , name="JobDetails")
 ]
--- job/views.py
+++ job/views.py
@@ -1,6 +1,7 @@
 from django.db.models.base import Model
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 import job
 from .models import Job,Category
@@ -8,8 +9,11 @@ from .models import Job,Category
 
 def ListJobs(request):
     list = Job.objects.all()
-    # return HttpResponse(list)
-    context={'jobs':list}
+    paginator = Paginator(list, 1) # Show 5 jobs per page.
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    # context={'jobs':list,'counts':len(list),'categoreis':Category.objects.all}
+    context={'jobs':page_obj,'counts':len(list),'categoreis':Category.objects.all}
     return render(request,"job/JobList.html",context=context)
 
 def JobDetails(request,id):
