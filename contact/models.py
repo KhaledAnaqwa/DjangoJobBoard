b2f5da26919e7736a5195986a81a1b99119e2b42
KhaledAnaqwa <khaled.anaqwa@gmail.com>
2021-11-27 15:16:15 +0200

    add contact

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,16 @@
+import email
 from django.db import models
-
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
+
+ 
+class Info (models.Model):
+    place=models.CharField(max_length=200)
+    phoneNumber=PhoneNumberField()
+    email = models.EmailField(max_length=100)
+
+    def __str__(self):
+        return self.email
+
+    # def get_absolute_url(self):
+    #     return reverse("_detail", kwargs={"pk": self.pk})
--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Info
 # Create your views here.
 
 def contact(request):
-    return HttpResponse("Hi")
+    info = Info.objects.get()
+    return render(request,'contact/contact.html',{"info":info})
