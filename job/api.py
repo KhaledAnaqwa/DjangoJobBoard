900ff11f333ef0a79fddd21c105fce8ed01d2adb
KhaledAnaqwa <khaled.anaqwa@gmail.com>
2021-11-28 10:39:58 +0200

    add apis using ViewSet + GenericView

--- job/api.py
+++ job/api.py
@@ -3,7 +3,9 @@ from .models import Job
 from .serializers import JobSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-# from rest_framework import generics
+from rest_framework import generics
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
 
 
 @api_view(['GET'])
@@ -12,3 +14,36 @@ def JobListApi(request):
     data = JobSerializer(all_data,many=True).data
 
     return Response(data)
+
+@api_view(['GET'])
+def JobDetailApi(request,id):
+    all_data = Job.objects.get(id__exact=id)
+    data = JobSerializer(all_data).data
+
+    return Response(data)
+
+
+class JobListApiV2(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+    lookup_field='id'
+
+
+
+class JobViewSet(viewsets.ViewSet):
+    """
+    A simple ViewSet for listing or retrieving jobs.
+    """
+    def list(self, request):
+        queryset = Job.objects.all()
+        serializer = JobSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Job.objects.all()
+        job = get_object_or_404(queryset, pk=pk)
+        serializer = JobSerializer(job)
+        return Response(serializer.data)
+    
+
+    
--- job/urls.py
+++ job/urls.py
@@ -1,10 +1,24 @@
 from django.urls import path,include
 from . import views,api
 
+user_list = api.JobViewSet.as_view({'get': 'list'})
+user_detail = api.JobViewSet.as_view({'get': 'retrieve'})
+
 app_name="job"
 urlpatterns = [
     path('',views.ListJobs,name="JobList"),
+    #api using functions 
+    
     path('api/list',api.JobListApi,name="JobListApi"),
+    path('api/jobdetail/<int:id>',api.JobDetailApi,name="JobDetailApi"),
+    
+    #api using geniraic view
+    path('api/v2/list/<int:id>',api.JobListApiV2.as_view(),name="JobListApiV2"),
+    
+    # api using viewset
+    path('api/v3/job/<int:pk>',user_detail,name="JobDetailApiV3"),
+    path('api/v3/list/',user_list,name="JobListApiV3"),
+    
     path('add',views.AddJob,name="AddJob"),
     path('<str:slug>',views.JobDetails , name="JobDetails")
 ]
